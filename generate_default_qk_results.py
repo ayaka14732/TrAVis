273d9033df7fc53e55004a0926ec5504c5a4809a
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-09-27 14:15:51 +0800

    update python script

--- generate_default_qk_results.py
+++ generate_default_qk_results.py
@@ -2,20 +2,13 @@ import struct
 
 from main import run
 
-def flatten(xs):
-    def inner():
-        for x in xs:
-            if not isinstance(x, list):
-                yield x
-            else:
-                yield from flatten(x)
-    return list(inner())
+def flatten4d(xssss):
+    return [x for xsss in xssss for xss in xsss for xs in xss for x in xs]
 
 s = 'A wealthy white-collar worker from Costa Rica working in our marketing agency has set up a marine conservation centre.'
 qk_results, tokens = run(s)
-qk_results = flatten(qk_results)
-len_qk_results = len(qk_results)
-b = struct.pack(f'<{len_qk_results}f', *qk_results)
+qk_results = flatten4d(qk_results)
+b = struct.pack(f'<{len(qk_results)}f', *qk_results)
 
 with open('default_qk_results.dat', 'wb') as f:
     f.write(b)
